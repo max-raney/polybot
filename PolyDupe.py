002d289f73e29048ddff0979109121953b9906b0
max-raney <max@kraney.com>
2023-10-28 11:18:12 -0700

    Made coordinate finder function with optimized matrix multiplication

--- PolyDupe.py
+++ PolyDupe.py
@@ -11,32 +11,40 @@ class Map:
         # Initialize the map as a 2D array of tile types
         self.map = np.empty((size, size), dtype=str)
         self.size = size
+        self.troop = None
 
         for x in range(size):
             for y in range(size):
                 # Initialize each tile as empty
                 self.map[x][y] = TILE_EMPTY
 
-    def draw(self, screen, tile_image, screen_width, screen_height):
+    def draw(self, screen, tile_image, screen_dims):
         # Define the size of each tile (image size)
-        tile_height = screen_height / self.size * 1.25
-        tile_width = screen_height / self.size * 1.25
+        tile_size = np.array([tile_image.get_width(), tile_image.get_height()]) // 10
 
-        for x in range(len(self.map) -1, -1, -1):
-            for y in range(len(self.map[x])):
+        for x in range(self.size-1, -1, -1):
+            for y in range(self.size):
                 # Calculate the position of the tile on the screen
                 # Adjust the x and y coordinates based on the map's shape
-                tile_x = ((x + y) / 2) * 0.95 * tile_width
-                tile_y = ((y - x) / 2) * tile_height * 0.6 + screen_height / 2
+                tile_coords = self.index_to_pixel(x, y, screen_dims, tile_size)
 
                 # Get the type of the current tile
                 tile_type = self.map[x][y]
 
                 # Scale down the tile image
-                scaled_tile = pygame.transform.scale(tile_image, (tile_height, tile_height))
+                scaled_tile = pygame.transform.scale(tile_image, tile_size.astype(int))
 
                 # Draw the scaled tile image at the calculated position
-                screen.blit(scaled_tile, (tile_x, tile_y))
+                screen.blit(scaled_tile, tile_coords.T[0])
+    
+    def index_to_pixel(self, x, y, screen_dims, tile_size, offset = 1):
+        A = np.array([[0.4975, 0.4975],[-0.31, 0.31]])
+        coordinate = np.array([[x], [y]])
+        offset_v = np.array([[offset], [offset + screen_dims[1]/2]])
+        resize_matrix = np.diag(tile_size)
+        transformation_matrix = resize_matrix @ A
+
+        return np.floor(resize_matrix @ (A @ coordinate) + offset_v).astype(int)
 
 
 class Tile():
@@ -47,18 +55,27 @@ class City():
     ''
 
 class Troop():
-    ''
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 class Warrior(Troop):
-    ''
+    attack = 2
+    defense = 2
+    movement = 1
+    range = 1
+    
+    def __init__(self, x, y):
+        self.health = 10
+        super(x, y)
 
 def main():
     # Initialize Pygame
     pygame.init()
 
     # Define screen dimensions
-    screen_width = 800
-    screen_height = 600
+    screen_width = 1600
+    screen_height = 900
 
     # Create a Pygame window
     screen = pygame.display.set_mode((screen_width, screen_height))
@@ -67,7 +84,7 @@ def main():
     pygame.display.set_caption("Fake Poly")
 
     # Load your custom tile image
-    tile_image = pygame.image.load("Art/Grass.webp")
+    tile_image = pygame.image.load("Art/Terrain/Tiles/ground_1.png")
 
     # Create a map with a size of 10x10 (adjust the size as needed)
     game_map = Map(10)
@@ -85,7 +102,7 @@ def main():
         screen.fill((255, 255, 255))
 
         # Draw the map
-        game_map.draw(screen, tile_image, screen_width, screen_height)
+        game_map.draw(screen, tile_image, (screen_width, screen_height))
 
         # Update the display
         pygame.display.flip()
