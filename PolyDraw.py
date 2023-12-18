5e93da88bef04de1bca0930652a587dcc01d5d08
max-raney <max@kraney.com>
2023-12-17 18:28:56 -0800

    New Enum for land types (much slower!)

--- PolyDraw.py
+++ PolyDraw.py
@@ -1,33 +1,47 @@
 # PolyDraw.py
 import pygame
 import numpy as np
-from PolyEntities import Tile, City, Unit
+from PolyEntities import Map, Tile
+
 
 def scaled_size(image, dimensions):
     return np.array([image.get_width(), image.get_height()]) // dimensions['scale']
 
-def index_to_pixel(x, y, dimensions, offset = [0,0]):
-    A = np.array([[0.4975, 0.4975],[-0.31, 0.31]])
+
+def index_to_pixel(x, y, dimensions, offset=[0, 0]):
+    A = np.array([[0.4975, 0.4975], [-0.31, 0.31]])
     coordinate = np.array([[x], [y]])
-    offset_v = np.array([[dimensions['camera_offset'][0] + offset[0]], [dimensions['camera_offset'][1] + offset[1] + dimensions['screen_dims'][1]/2]])
+    offset_v = np.array([[dimensions['camera_offset'][0] + offset[0]],
+                         [dimensions['camera_offset'][1] + offset[1] + dimensions['screen_dims'][1] / 2]])
     resize_matrix = np.diag(dimensions['tile_size'])
 
     return np.floor(resize_matrix @ (A @ coordinate) + offset_v).astype(int)
 
+
 class Drawer:
+
+    IMAGE_PATHS = {
+        Tile.LandType.GRASS: "Art/Terrain/Tiles/ground_1.png",
+        Tile.LandType.WATER: "Art/Terrain/Tiles/ground_10.png",
+    }
+
     def __init__(self, dimensions, map):
         self.dimensions = dimensions
         self.map = map
 
     def _draw_map(self):
+
         map_surface = pygame.Surface(self.dimensions['screen'].get_size())
 
-        for x in range(self.map.size-1, -1, -1):
+        for x in range(self.map.size - 1, -1, -1):
             for y in range(self.map.size):
                 # Calculate the position of the tile on the screen
                 tile_coords = index_to_pixel(x, y, self.dimensions)
 
-                scaled_tile = pygame.transform.scale(self.map.tiles[x][y].image, self.dimensions['tile_size'].astype(int))
+                scaled_tile = pygame.transform.scale(
+                    pygame.image.load(self.IMAGE_PATHS[self.map.tiles[x][y].land_type]),
+                    self.dimensions['tile_size'].astype(int)
+                )
 
                 # Draw the scaled tile image at the calculated position
                 map_surface.blit(scaled_tile, tile_coords.T[0])
--- PolyEntities.py
+++ PolyEntities.py
@@ -1,8 +1,11 @@
 # PolyEntities
 import pygame
 import numpy as np
+from enum import Enum
+
 
 class Map:
+
     def __init__(self, size):
         # Initialize the map as a 2D array of tile types
         self.tiles = np.empty((size, size), dtype=Tile)
@@ -11,35 +14,40 @@ class Map:
 
         for x in range(size):
             for y in range(size):
-                # self.tiles[x][y] = Tile('grass')
                 if (x + y) % 2 == 0:
-                    self.tiles[x][y] = Tile('grass')
+                    self.tiles[x][y] = Tile(Tile.LandType.GRASS)
                 else:
-                    self.tiles[x][y] = Tile('water')
+                    self.tiles[x][y] = Tile(Tile.LandType.WATER)
 
     def add_unit(self, unit, x, y):
         self.tiles[x][y].troop = unit
         self.units.append(unit)
 
+
 class Tile:
+    class LandType(Enum):
+        GRASS = 'grass'
+        WATER = 'water'
+
     def __init__(self, land_type):
         self.land_type = land_type
-        if land_type == 'grass':
-            self.image = pygame.image.load("Art/Terrain/Tiles/ground_1.png")
-        else:
-            self.image = pygame.image.load("Art/Terrain/Tiles/ground_10.png")
+        self.troop = None
+        self.city = False
+
 
 class City:
     # Add City class details if needed
     pass
 
+
 class Unit:
     unit_image = pygame.image.load('Art/Units/Imperius/Default/Imperius_Default_Scout.png')
-    
+
     def __init__(self, x, y, map):
         self.x = x
         self.y = y
         map.add_unit(self, x, y)
+
 
 class Warrior(Unit):
     attack = 2
@@ -48,19 +56,7 @@ class Warrior(Unit):
     range = 1
 
     unit_image = pygame.image.load('Art/Units/Imperius/Default/Imperius_Default_Warrior.png')
-    
+
     def __init__(self, x, y, map):
         self.health = 10
         super().__init__(x, y, map)
-
-
-def scaled_size(image, dimensions):
-    return np.array([image.get_width(), image.get_height()]) // dimensions['scale']
-
-def index_to_pixel(x, y, dimensions, offset = [0,0]):
-    A = np.array([[0.4975, 0.4975],[-0.31, 0.31]])
-    coordinate = np.array([[x], [y]])
-    offset_v = np.array([[dimensions['camera_offset'][0] + offset[0]], [dimensions['camera_offset'][1] + offset[1] + dimensions['screen_dims'][1]/2]])
-    resize_matrix = np.diag(dimensions['tile_size'])
-
-    return np.floor(resize_matrix @ (A @ coordinate) + offset_v).astype(int)
